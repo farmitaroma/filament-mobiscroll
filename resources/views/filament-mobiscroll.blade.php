3e24a9606a872ee8ca8ef1b4d831287ccb37a50f
MatteoTorelli <m.torelli@farm.it>
2024-02-27 16:24:31 +0100

    Fixed package name

--- resources/views/filament-mobiscroll.blade.php
+++ resources/views/filament-mobiscroll.blade.php
@@ -1,8 +1,8 @@
 <x-filament-widgets::widget>
     <x-filament::section>
         <div id="event-calendar" wire:ignore ax-load
-             ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-mobiscroll-alpine', 'farm/filament-mobiscroll') }}"
-             ax-load-css="{{ \Filament\Support\Facades\FilamentAsset::getStyleHref('filament-mobiscroll-alpine', 'farm/filament-mobiscroll') }}"
+             ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('filament-mobiscroll-alpine', 'farmit/filament-mobiscroll') }}"
+             ax-load-css="{{ \Filament\Support\Facades\FilamentAsset::getStyleHref('filament-mobiscroll-alpine', 'farmit/filament-mobiscroll') }}"
              x-ignore
              x-data="filamentMobiscroll()"
         >
